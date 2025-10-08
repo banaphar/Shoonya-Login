05412ee2ee9bfceea572281d946f6a0c8e4b174b
vivek singh <thebanaphar@gmail.com>
2025-10-08 15:33:03 +0530

    Delete package.sh

--- package.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-pkg update -y && pkg upgrade -y && pkg install -y python clang make cmake ninja patchelf && export PATH=$PATH:/data/data/com.termux/files/usr/bin && pip install --upgrade pip setuptools wheel && pip install numpy pandas PyYAML pyotp NorenRestApiPy norenrestapi
